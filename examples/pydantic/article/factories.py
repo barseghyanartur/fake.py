c88577d07668411729f33108cdc928ad773e0cd1
Artur Barseghyan <artur.barseghyan@gmail.com>
2023-11-30 18:51:42 +0100

    More

---
+++ examples/pydantic/article/factories.py
@@ -0,0 +1,17 @@
+from fake import Factory, PydanticModelFactory
+
+from article.models import Article
+
+
+class ArticleFactory(PydanticModelFactory):
+    title = Factory.sentence()
+    slug = Factory.slug()
+    content = Factory.text()
+    # image = Factory.png_file(storage=STORAGE)
+    pub_date = Factory.date()
+    safe_for_work = Factory.pybool()
+    minutes_to_read = Factory.pyint(min_value=1, max_value=10)
+    # author = SubFactory(UserFactory)
+
+    class Meta:
+        model = Article
