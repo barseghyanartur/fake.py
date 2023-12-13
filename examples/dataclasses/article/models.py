d206d45124b94986b75d7282f31b7e76aefdf578
Artur Barseghyan <artur.barseghyan@gmail.com>
2023-12-13 01:25:31 +0100

    Fixes

--- examples/dataclasses/article/models.py
+++ examples/dataclasses/article/models.py
@@ -37,7 +37,7 @@ class Article:
     content: str
     author: User
     image: Optional[str] = None  # Use str to represent the image path or URL
-    pub_date: datetime = date.today()
+    pub_date: date = date.today()
     safe_for_work: bool = False
     minutes_to_read: int = 5
 
