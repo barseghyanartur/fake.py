57100273dd22f3550ac9888f44471bd655b15797
Artur Barseghyan <artur.barseghyan@gmail.com>
2024-06-09 17:24:58 +0200

    More

--- examples/sqlalchemy/article/factories.py
+++ examples/sqlalchemy/article/factories.py
@@ -1,13 +1,17 @@
 from pathlib import Path
+from typing import Any, Dict
 
 from fake import (
     FACTORY,
+    FAKER,
     FileSystemStorage,
     PostSave,
+    PreInit,
     PreSave,
     SQLAlchemyModelFactory,
     SubFactory,
     post_save,
+    pre_init,
     pre_save,
     trait,
 )
@@ -27,6 +31,20 @@ __all__ = (
 BASE_DIR = Path(__file__).resolve().parent.parent
 MEDIA_ROOT = BASE_DIR / "media"
 STORAGE = FileSystemStorage(root_path=MEDIA_ROOT, rel_path="tmp")
+CATEGORIES = (
+    "art",
+    "technology",
+    "literature",
+)
+TAGS = (
+    "painting",
+    "photography",
+    "ai",
+    "data-engineering",
+    "fiction",
+    "poetry",
+    "manual",
+)
 
 
 def get_session():
@@ -67,10 +85,22 @@ def add_to_group(user: User, name: str) -> None:
         session.commit()  # Commit the changes
 
 
+def slugify(text: str) -> str:
+    slug = text.replace(" ", "-").replace("_", "-")
+    slug = "".join(char for char in slug if char.isalnum() or char == "-")
+    return slug.lower()
+
+
+def set_username(data: Dict[str, Any]) -> None:
+    first_name = slugify(data["first_name"])
+    last_name = slugify(data["last_name"])
+    data["username"] = f"{first_name}_{last_name}_{FAKER.pystr().lower()}"
+
+
 class UserFactory(SQLAlchemyModelFactory):
     """User factory."""
 
-    username = FACTORY.username()  # type: ignore
+    username = PreInit(set_username)  # type: ignore
     first_name = FACTORY.first_name()  # type: ignore
     last_name = FACTORY.last_name()  # type: ignore
     email = FACTORY.email()  # type: ignore
@@ -106,23 +136,35 @@ class UserFactory(SQLAlchemyModelFactory):
         instance._post_save_called = True
 
 
+def set_headline(data: Dict[str, Any]) -> None:
+    data["headline"] = data["content"][:25]
+
+
 class ArticleFactory(SQLAlchemyModelFactory):
     """Article factory."""
 
     title = FACTORY.sentence()  # type: ignore
     slug = FACTORY.slug()  # type: ignore
     content = FACTORY.text()  # type: ignore
+    headline = PreInit(set_headline)
+    category = FACTORY.random_choice(elements=CATEGORIES)
+    pages = FACTORY.pyint(min_value=1, max_value=100)  # type: ignore
     image = FACTORY.png_file(storage=STORAGE)  # type: ignore
     pub_date = FACTORY.date()  # type: ignore
     safe_for_work = FACTORY.pybool()  # type: ignore
     minutes_to_read = FACTORY.pyint(min_value=1, max_value=10)  # type: ignore
     author = SubFactory(UserFactory)  # type: ignore
+    tags = FACTORY.random_sample(elements=TAGS, length=3)
 
     class Meta:
         model = Article
 
     class MetaSQLAlchemy:
         get_session = get_session
+
+    @pre_init
+    def set_auto_minutes_to_read(self, data: Dict[str, Any]) -> None:
+        data["auto_minutes_to_read"] = data["pages"]
 
     @pre_save
     def _pre_save_method(self, instance):
