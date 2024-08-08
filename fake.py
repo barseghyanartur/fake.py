2cc9b045bbbffa7175292583e7e7183408a08acc
Artur Barseghyan <artur.barseghyan@gmail.com>
2024-08-09 01:30:26 +0200

    Prepare for namespaced packages (#164)
    * Prepare for namespaced packages

--- fake.py
+++ fake.py
@@ -55,7 +55,7 @@ from unittest.mock import patch
 from uuid import UUID
 
 __title__ = "fake.py"
-__version__ = "0.9.2"
+__version__ = "0.9.3"
 __author__ = "Artur Barseghyan <artur.barseghyan@gmail.com>"
 __copyright__ = "2023-2024 Artur Barseghyan"
 __license__ = "MIT"
