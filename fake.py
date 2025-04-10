46aa5c7fa6be107001e89cf28ab2b7016bd7fcc9
Artur Barseghyan <artur.barseghyan@gmail.com>
2025-04-11 00:22:26 +0200

    More

--- fake.py
+++ fake.py
@@ -67,7 +67,7 @@ from unittest.mock import MagicMock, patch
 from uuid import UUID
 
 __title__ = "fake.py"
-__version__ = "0.11"
+__version__ = "0.11.1"
 __author__ = "Artur Barseghyan <artur.barseghyan@gmail.com>"
 __copyright__ = "2023-2024 Artur Barseghyan"
 __license__ = "MIT"
